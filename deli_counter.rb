7ebd42b496b960e5a0488dae7b01818e2cecf4d3
abstract-animation-9914 <vm930@hotmail.com>
2019-01-02 14:52:55 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def line
+  puts "The line is currently empty."
+end
+
+def take_a_number(katz_deli, name)
+  puts "Welcome, #{name}. You are number #{katz_deli[0]+1} in line."
+end
+
+def now_serving
+  serving = katz_deli.unshift()
+  if katz_deli.size === 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{serving}."
+end
