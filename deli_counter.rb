115790b6743bae2421c17229341175f1d5a65993
abstract-animation-9914 <vm930@hotmail.com>
2019-01-02 14:51:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,4 +7,10 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli[0]+1} in line."
 end
 
-
+def now_serving
+  serving = katz_deli.unshift()
+  if katz_deli.size === 0
+    puts ""
+  else
+    
+end
